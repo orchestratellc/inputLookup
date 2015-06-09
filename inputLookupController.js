9b2f20366fcfb3a22eed6e2bbe3637f93fa85bea
Sayer Martin <sayer@sayerm.com>
2015-06-09 08:53:03 -0500

    Create inputLookupController.js

---
+++ inputLookupController.js
@@ -0,0 +1,27 @@
+({
+    doInit: function(cmp,event,helper) {
+        // get name for passed-in lookup id (optional attribute) if we don't have a name
+        console.log('initial input value');
+        console.log(cmp.get('v.value'));
+        var cId = cmp.get('v.value');
+        if (cId) {
+            helper.searchById(cmp);
+        }
+    },
+    handleIdInput: function(cmp) {
+        var cId = cmp.get('v.value');
+        var cName = cmp.get('v.searchTerm');
+        //console.log('cId '+cId);
+        //console.log('cName '+cName);        
+        if (cId && !cName) {
+            helper.searchById(cmp);
+        }        
+    },
+    handleSearchChange : function(cmp, event, helper) {
+        if (cmp.get('v.searchTerm') && cmp.get('v.searchTerm').length>1)
+			helper.searchByName(cmp);
+	},
+    handleResultSelect: function(cmp,event,helper) {
+		helper.setResultsInfo(cmp,event);         
+    }
+})
